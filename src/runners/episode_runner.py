20c69cbee8c0c3ff85a92c3797aeed89d3130eab
tdanino12 <36441678+tdanino12@users.noreply.github.com>
2023-12-30 00:07:18 +0200

    Update episode_runner.py

--- src/runners/episode_runner.py
+++ src/runners/episode_runner.py
@@ -2,6 +2,7 @@ from envs import REGISTRY as env_REGISTRY
 from functools import partial
 from components.episode_buffer import EpisodeBatch
 import numpy as np
+import wandb
 
 
 class EpisodeRunner:
@@ -117,6 +118,7 @@ class EpisodeRunner:
     def _log(self, returns, stats, prefix):
         self.logger.log_stat(prefix + "return_mean", np.mean(returns), self.t_env)
         self.logger.log_stat(prefix + "return_std", np.std(returns), self.t_env)
+        wandb.log({"reward": np.mean(returns)})
         returns.clear()
 
         for k, v in stats.items():
